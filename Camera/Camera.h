c9643b806b43dab93bba27e555eac339f2976aac
Austin Morrell <austinlm19@gmail.com>
2016-08-31 12:59:05 -0500

    Started the camera class

---
+++ Camera/Camera.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "src/gl_core_4_4.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+#define GLM_SWIZZLE
+#define GLM_FORCE_PURE
+#include "src\Gizmos.h"
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat4;
+using namespace std;
+
+class Camera
+{
+
+};
--- Camera/Source.cpp
+++ Camera/Source.cpp
@@ -1,16 +1,4 @@
-#include "src/gl_core_4_4.h"
-#include <GLFW/glfw3.h>
-#include <iostream>
-#define GLM_SWIZZLE
-#define GLM_FORCE_PURE
-#include "src\Gizmos.h"
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
-
-using glm::vec3;
-using glm::vec4;
-using glm::mat4;
-using namespace std;
+#include "Camera.h"
 
 int main()
 {
