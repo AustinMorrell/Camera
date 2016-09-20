437fcd7d7b348b61bf9086b0048ad77ac8c33ef3
Austin Morrell <austinlm19@gmail.com>
2016-09-20 13:47:01 -0500

    Done, it can move with mouse now!

--- Camera/Camera.cpp
+++ Camera/Camera.cpp
@@ -41,6 +41,13 @@ void Camera::updateProjectionViewTransform()
 	projectionViewTransform = projectionTransform * viewTransform * worldTransform;
 }
 
+FlyCamera::FlyCamera()
+{
+	sbMouseButtonDown = true;
+	siPrevMouseX = 0;
+	siPrevMouseY = 0;
+}
+
 void FlyCamera::setSpeed(float speed)
 {
 	this->speed = speed;
@@ -48,110 +55,74 @@ void FlyCamera::setSpeed(float speed)
 
 void FlyCamera::update(double deltaTime)
 {
-	mat4 Rotation, Scale, Trans = mat4(1);
 	GLFWwindow* wind = glfwGetCurrentContext();
 
-	float f_speed = glfwGetKey(wind, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ? deltaTime * speed * 2 : deltaTime * speed;
+	camRot, camScale, camTrans = mat4(1);
+
+	double f_speed = glfwGetKey(wind, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ? deltaTime * speed * 2 : deltaTime * speed;
 
 	if (glfwGetKey(wind, 'W') == GLFW_PRESS)
 	{
-		worldTransform[3] -= getWorldTransform()[2] * f_speed;
+		camTrans = glm::translate(vec3(0, 0, (-0.10f * deltaTime) * 10));
 	}
 	if (glfwGetKey(wind, 'S') == GLFW_PRESS)
 	{
-		worldTransform[3] += getWorldTransform()[2] * f_speed;
+		camTrans = glm::translate(vec3(0, 0, (0.10f * deltaTime) * 10));
 	}
 	if (glfwGetKey(wind, 'D') == GLFW_PRESS)
 	{
-		worldTransform[3] += getWorldTransform()[0] * f_speed;
+		camTrans = glm::translate(vec3((0.10f * deltaTime) * 10, 0, 0));
 	}
 	if (glfwGetKey(wind, 'A') == GLFW_PRESS)
 	{
-		worldTransform[3] -= getWorldTransform()[0] * f_speed;
-	}
-	if (glfwGetKey(wind, 'Q') == GLFW_PRESS)
-	{
-		worldTransform[3] += getWorldTransform()[1] * f_speed;
-	}
-	if (glfwGetKey(wind, 'E') == GLFW_PRESS)
-	{
-		worldTransform[3] -= getWorldTransform()[1] * f_speed;
+		camTrans = glm::translate(vec3((-0.10f * deltaTime) * 10, 0, 0));
 	}
 
-	//if (glfwGetMouseButton(wind, GLFW_MOUSE_BUTTON_LEFT))
-	//{
-	//	double preMposX = 0, preMposY = 0;
-
-	//	glfwGetCursorPos(wind, &preMposX, &preMposY);
-
-	//	double MposX = 0, MposY = 0;
-	//	glfwGetCursorPos(wind, &MposX, &MposY);
-
-	//	double deltaX = MposX - preMposX, deltaY = MposY - preMposY;
-	//	preMposX = MposX;
-	//	preMposY = MposY;
-
-	//	if (deltaY != 0)
-	//	{
-	//		thata = deltaY;
-	//		Rotation[1][1] = cos(thata);
-	//		Rotation[1][2] = -1 * sin(thata);
-	//		Rotation[2][1] = sin(thata);
-	//		Rotation[2][2] = cos(thata);
-	//	}
-
-	//	if (deltaX != 0)
-	//	{
-	//		thata = deltaX;
-	//		Rotation[0][0] = cos(thata);
-	//		Rotation[0][2] = sin(thata);
-	//		Rotation[2][0] = -1 * sin(thata);
-	//		Rotation[2][2] = cos(thata);
-	//	}
-	//	//worldTransform = glm::inverse(Trans * Rotation * Scale);
-	//}
-
-	static bool sbMouseButtonDown = false;
 	if (glfwGetMouseButton(wind, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS) {
 
-		static double siPrevMouseX = 0;
-		static double siPrevMouseY = 0;
-
-		if (sbMouseButtonDown == false) {
-			sbMouseButtonDown = true;
-			glfwGetCursorPos(wind, &siPrevMouseX, &siPrevMouseY);
-		}
-
 		double mouseX = 0, mouseY = 0;
+		float theta = 0.0f;
+
 		glfwGetCursorPos(wind, &mouseX, &mouseY);
 
-		double iDeltaX = mouseX - siPrevMouseX;
-		double iDeltaY = mouseY - siPrevMouseY;
+		if (sbMouseButtonDown)
+		{
+			siPrevMouseX = mouseX;
+			siPrevMouseY = mouseY;
+			sbMouseButtonDown = false;
+		}
+
+		double iDeltaX = siPrevMouseX - mouseX;
+		double iDeltaY = siPrevMouseY - mouseY;
+
+		iDeltaX *= 0.005f;
+		iDeltaY *= 0.005f;
+
+		if (iDeltaX < 1 || iDeltaX > 1)
+		{
+			theta = iDeltaX;
+			camRot[0][0] = cos(theta);
+			camRot[0][2] = sin(theta);
+			camRot[2][0] = -1.f * sin(theta);
+			camRot[2][2] = cos(theta);
+		}
+
+		if (iDeltaY < 1 || iDeltaY > 1)
+		{
+			theta = iDeltaY;
+			camRot[1][1] = cos(theta);
+			camRot[1][2] = -1.f * sin(theta);
+			camRot[2][1] = sin(theta);
+			camRot[2][2] = cos(theta);
+		}
 
 		siPrevMouseX = mouseX;
 		siPrevMouseY = mouseY;
-
-		glm::mat4 mMat;
-
-		// pitch
-		if (iDeltaY != 0) {
-			mMat = glm::axisAngleMatrix(m_transform[0].xyz(), (float)-iDeltaY / 150.0f);
-			m_transform[0] = mMat * m_transform[0];
-			m_transform[1] = mMat * m_transform[1];
-			m_transform[2] = mMat * m_transform[2];
-		}
-
-		// yaw
-		if (iDeltaX != 0) {
-			mMat = glm::axisAngleMatrix(m_up, (float)-iDeltaX / 150.0f);
-			m_transform[0] = mMat * m_transform[0];
-			m_transform[1] = mMat * m_transform[1];
-			m_transform[2] = mMat * m_transform[2];
-		}
 	}
 	else {
-		sbMouseButtonDown = false;
+		sbMouseButtonDown = true;
 	}
 
+	viewTransform *= glm::inverse(camTrans * camRot * camScale);
 	getProjectionView();
 }
--- Camera/FlyCamera.h
+++ Camera/FlyCamera.h
@@ -6,7 +6,14 @@ private:
 	double thata;
 	float speed;
 	vec3 up;
+	bool sbMouseButtonDown;
+	double siPrevMouseX;
+	double siPrevMouseY;
+	glm::mat4 camRot;
+	mat4 camTrans;
+	mat4 camScale;
 public:
+	FlyCamera();
 	void setSpeed(float speed);
 	void update(double deltaTime);
 };
